2bac78f8eac252f0ca7f9de61e2d84c3f54ea269
Chris MacGeorge <christopher.s.macgeorge@gmail.com>
2020-10-10 13:26:00 -0700

    routes

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require("express");
 // Path for filenames paths
 const path = require("path");
 // fs to read and write files
-const fs = require("fs")
+const fs = require("fs");
+const { get } = require("http");
 
 // Create an express server
 const app = express();
@@ -14,5 +15,18 @@ const PORT = process.env.PORT || 8000;
 Let notesData = [];
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, "./public"))) // STUCK HERE
+
+
+app.get("/api/notes", function(err, res) {
+    try {
+        notesData = fs.readFileSync("db/db.json", "utf8");
+        console.log("Success");
+        notesData = JSON.parse(notesData);
+    } catch (err) {
+        console.log("\n error (in app.get.catch):");
+        console.log(err);
+    }
+    res.json(notesData);
+});
